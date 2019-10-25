5998178aa26baa515ea4cc48f18ebe99bf85c63c
Andrii Ilchuk <ailchuk@e2r5p10.unit.ua>
2019-10-25 16:10:12 +0300

    added get_char_index

--- src/mx_get_char_index.c
+++ src/mx_get_char_index.c
@@ -14,13 +14,3 @@ int mx_get_char_index(const char *str, char c)
     else 
         return -2;
 }
-
-int main() 
-{
-    char *s = "Oleh";
-    char *a = NULL;
-    printf("%d\n", mx_get_char_index(s, 'l'));
-    printf("%d\n", mx_get_char_index(s, 'W')); // Ok
-    printf("%d\n", mx_get_char_index(s, *a));
-
-}
