193501c759b627fb54e521c4b365bd7890b0adc1
Andrii Ilchuk <ailchuk@e2r5p10.unit.ua>
2019-11-03 12:54:29 +0200

    mx_nbr_to_hex

---
+++ src/mx_nbr_to_hex.c
@@ -0,0 +1,39 @@
+#include "libmx.h"
+
+char *mx_nbr_to_hex(unsigned long nbr)
+{
+    unsigned long tmp = nbr;
+    int len = 0;
+    char *res = NULL;
+    int i = 0;
+
+    if (nbr == 0)
+        return "0";
+    while (tmp > 0)
+    {
+        tmp /= 16;
+        len++;
+    }
+    res = mx_strnew(len);
+    i = len - 1;
+    while (nbr > 0) 
+    {
+        int tmp = nbr % 16;
+
+        if (tmp < 10) {
+            res[i] = tmp + '0';
+        }
+        else {
+            res[i] = tmp + 87;
+        }
+        nbr /= 16;
+        i--;
+    }
+    return res;
+}
+
+int main() 
+{
+    printf("%s\n", mx_nbr_to_hex(52)); //returns "34"
+    printf("%s\n", mx_nbr_to_hex(1000)); //returns "3e8"; //returns 0
+}
