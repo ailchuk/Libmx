042298f4bf116e9e61e02cdd295d61437615215c
Andrii Ilchuk <ailchuk@e2r5p10.unit.ua>
2019-11-03 12:56:04 +0200

    strtrim

--- src/mx_strtrim.c
+++ src/mx_strtrim.c
@@ -5,28 +5,28 @@ char *mx_strtrim(const char *str)
 
     if (!str)
         return NULL;
-    int leading = 0;
-    int trailing = 0;
+    int left = 0;
+    int right = 0;
     int i;
     int length = mx_strlen(str);
     char *result;
 
     for (i = 0; mx_isspace(str[i]); i++)
     {
-        leading++;
-        if (leading == mx_strlen(str))
+        left++;
+        if (left == mx_strlen(str))
             return "\0";
     }
     for (i = length - 1; mx_isspace(str[i]); i--)
-        trailing++;
-    result = mx_strnew(length - leading - trailing);
-    mx_strncpy(result, str + leading, length - leading - trailing);
+        right++;
+    result = mx_strnew(length - left - right);
+    mx_strncpy(result, str + left, length - left - right);
     return result;
 }
 
 int main()
 {
-    //char *name = "\f My name... is \r Neo \t\n ";
+    //char *name = "\f My name... is Neo \t\n ";
     char *name = "     ";
     printf("%s", mx_del_extra_spaces(name)); //returns "My name... is Neo" 
     //char *arr = mx_del_extra_spaces(name);
