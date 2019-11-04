ee207532457cff8afb3c092c77540fa6bcf34902
ailchuk <an.ilchuk27@gmail.com>
2019-11-04 22:22:17 +0200

    added memccpy

--- src/mx_memccpy.c
+++ src/mx_memccpy.c
@@ -2,24 +2,14 @@
 
 void *mx_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
 {
-    char *dst1 = (char *)dst;
-    char *src1 = (char *)src;
+	unsigned char *dest = (unsigned char *)dst;
+	unsigned char *source = (unsigned char *)src;
 
-    for (size_t i = 0; i < n; dst1++, src1++, i++)
-    {
-        *dst1 = *src1;
-        if (*src1 == (char)c)
-            return (dst1 + 1);       
-    }
-    return 0;
-}
-
-int main () {
-     // Массив источник данных
-   char src[15] = "1234567890";
-
-   // Массив приемник данных
-   char dst[15] = "";
-
-    printf("%s\n", mx_memccpy(dst, src, '5', 10));
+	for (size_t i = 0; i < n; dest++, source++, i++)
+	{
+		*dest = *source;
+		if (*source == (unsigned char)c)
+			return (dest + 1);
+	}
+	return (0);
 }
