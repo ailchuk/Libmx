fe51653efea81eb58bcbcac777f58381e3615640
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 15:07:08 +0200

    Update mx_strtrim.c

--- src/mx_strtrim.c
+++ src/mx_strtrim.c
@@ -1,13 +1,11 @@
 #include "libmx.h"
-char *mx_strtrim(const char *str)
-{
+char *mx_strtrim(const char *str) {
     if (!str)
         return NULL;
     int len = mx_strlen(str);
     while (mx_isspace(str[len - 1]) && len > 0)
         len--;
-    while (mx_isspace(*str) && len > 0)
-    {
+    while (mx_isspace(*str) && len > 0) {
         str++;
         len--;
     }
