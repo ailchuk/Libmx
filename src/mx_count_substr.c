0fb854116bde8b29fcb9a8eada772f17a3a318ea
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 14:41:20 +0200

    Update mx_count_substr.c

--- src/mx_count_substr.c
+++ src/mx_count_substr.c
@@ -1,7 +1,6 @@
 #include "libmx.h"
 
-int mx_count_substr(const char *str, const char *sub)
-{
+int mx_count_substr(const char *str, const char *sub) {
     if (!str || !sub)
         return -1;
     int len = mx_strlen(sub);
