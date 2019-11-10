cabf87d25b7a1bffa88aaeb1c454a56b4dbd1414
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 14:40:31 +0200

    Update mx_binary_search.c

--- src/mx_binary_search.c
+++ src/mx_binary_search.c
@@ -1,14 +1,12 @@
 #include "libmx.h"
 
-int mx_binary_search(char **arr, int size, const char *s, int *count)
-{
+int mx_binary_search(char **arr, int size, const char *s, int *count) {
     int first = 0;
     int last = size - 1;
     int mid = (first + last) / 2;
     *count = 0;
 
-    while (first <= last)
-    {
+    while (first <= last) {
         *count += 1;
         if (mx_strcmp(arr[mid], s) < 0)
             first = mid + 1;
