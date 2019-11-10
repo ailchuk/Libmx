a242050c0d63f10196e142c3c6c8dde9913f6ad7
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 22:53:32 +0200

    Update mx_atoi.c

--- src/mx_atoi.c
+++ src/mx_atoi.c
@@ -1,14 +1,21 @@
 #include "libmx.h"
 
 int mx_atoi(const char *str) {
-    int i = -1;
-    int res = 0;
-
-    while (str[++i]) {
-        if (mx_isdigit(str[i]))
-            res = res * 10 + str[i] - '0';
-        if (!mx_isdigit(str[i]))
-            return res;
+    int i = 0;
+    int n = 0;
+    int s = 1;
+  
+    while (!mx_isdigit(str[i])){
+        if (!mx_isspace(str[i]) && str[i] != '-' && str[i] != '+')
+            return 0;
+        i++;
     }
-    return res;
+    if (str[i - 1] == '-')
+        s = -s;
+    for (; mx_isdigit(str[i]); i++)
+        n = ((n * 10) + (str[i] - '0'));
+    if (s < 0)
+        n = -n;
+    return n;
 }
+
