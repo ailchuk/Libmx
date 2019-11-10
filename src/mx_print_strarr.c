0eea76d7afba66027606b154b95b6cb6e8863100
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 14:57:28 +0200

    Update mx_print_strarr.c

--- src/mx_print_strarr.c
+++ src/mx_print_strarr.c
@@ -1,14 +1,10 @@
 #include "libmx.h"
 
-void mx_print_strarr(char **arr, const char *delim)
-{
-    if (*arr != NULL && *delim)
-    {
+void mx_print_strarr(char **arr, const char *delim) {
+    if (*arr != NULL && *delim) {
         int i = 0;
-        while (arr[i] != NULL)
-        {
-            if (arr[i + 1])
-            {
+        while (arr[i] != NULL) {
+            if (arr[i + 1]) {
                 mx_printstr(arr[i]);
                 mx_printchar(*delim);
             }
