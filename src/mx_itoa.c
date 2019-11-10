b29cf938c594aed30e00f0c7d1b66d599007062b
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 14:52:19 +0200

    Update mx_itoa.c

--- src/mx_itoa.c
+++ src/mx_itoa.c
@@ -4,21 +4,18 @@ static int i_len(int n)
 {
     int i = 0;
 
-    if (n < 0)
-    {
+    if (n < 0) {
         n *= -1;
         i++;
     }
-    if (n > 0)
-    {
+    if (n > 0) {
         for(;n / 10 != 0; i++)
             n /= 10;
     }
     return i + 1;
 }
 
-char *mx_itoa(int number)
-{
+char *mx_itoa(int number) {
     long long int n = number;
     size_t len = i_len(n);
     char *str = mx_strnew(len);
@@ -29,13 +26,11 @@ char *mx_itoa(int number)
         return mx_strdup("-2147483648");
     if (n == 0)
         str[0] = 48;
-    if (n < 0)
-    {
+    if (n < 0) {
         str[0] = '-';
         n *= -1;
     }
-    while (n > 0)
-    {
+    while (n > 0) {
         str[--len] = (n % 10) + 48;
         n /= 10;
     }
