442bef4b984f87b032f4961e51fe3d99d07acb7a
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 15:06:11 +0200

    Update mx_strsplit.c

--- src/mx_strsplit.c
+++ src/mx_strsplit.c
@@ -1,28 +1,25 @@
 #include "libmx.h"
 
-char **mx_strsplit(const char *s, char c) 
-{
+char **mx_strsplit(const char *s, char c) {
     if (!s || mx_get_char_index(s, c) == -1)
-      return NULL;
+        return NULL;
 
-  int size_arr = mx_count_words(s, c);
-  char **arr = malloc((sizeof(char *) * (size_arr + 1)));
-  int len = mx_strlen(s);
-  int id = 0;
-  int j = 0;
-  for (int i = 0; i < len; i++)
-  {
-      id = mx_get_char_index(s, c);
-      id = id == -1 ? mx_strlen(s) : id;
-      if (id)
-      {
-          arr[j] = mx_strndup(s, id);
-          s += mx_strlen(arr[j]) - 1;
-          i += mx_strlen(arr[j]) - 1;
-          j++;
-      }
-      s++;
-  }
-  arr[size_arr] = NULL;
-  return arr;
+    int size_arr = mx_count_words(s, c);
+    char **arr = malloc((sizeof(char *) * (size_arr + 1)));
+    int len = mx_strlen(s);
+    int id = 0;
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        id = mx_get_char_index(s, c);
+        id = id == -1 ? mx_strlen(s) : id;
+        if (id) {
+            arr[j] = mx_strndup(s, id);
+            s += mx_strlen(arr[j]) - 1;
+            i += mx_strlen(arr[j]) - 1;
+            j++;
+        }
+        s++;
+    }
+    arr[size_arr] = NULL;
+    return arr;
 }
