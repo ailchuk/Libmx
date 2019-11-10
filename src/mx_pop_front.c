e66b2f825f80723c072452bab4ef895f5e70ebda
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 14:56:38 +0200

    Update mx_pop_front.c

--- src/mx_pop_front.c
+++ src/mx_pop_front.c
@@ -1,21 +1,17 @@
 #include "libmx.h"
 
-void mx_pop_front(t_list **head)
-{
-    if (head == NULL) return;
-    
-    if((*head)->next == NULL)
-    {
+void mx_pop_front(t_list **head) {
+    if (head == NULL) 
+        return;
+    if((*head)->next == NULL) {
         free(*head);
         *head = NULL;
         return;
     }
-    else
-    {
+    else {
         t_list *ptr = (*head)->next;
         (*head)->data = NULL;
         free(*head);
         *head = ptr;
     }
-    
 }
