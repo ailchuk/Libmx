6cc36bc050166ec971942382cdd661ebc68ba5c8
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 22:13:05 +0200

    Update mx_realloc.c

--- src/mx_realloc.c
+++ src/mx_realloc.c
@@ -1,16 +1,27 @@
 #include "libmx.h"
 
 void *mx_realloc(void *ptr, size_t size) {
-    void *result = NULL;
-
-    if (!ptr && size)
-        return malloc(size);
-    if (!size && ptr) {
+    void *result;
+    if (size == 0 && ptr) {
         free(ptr);
         return NULL;
     }
-    result = malloc(size);
-    mx_memcpy(result, ptr, malloc_size(ptr));
-    free(ptr);
+    if (size && !ptr)
+        return malloc(size);
+    size_t length = mx_strlen(ptr);
+
+    if (length > size) {
+        result = malloc(length);
+        if (!result)
+            return NULL;
+        result = mx_memmove(result, ptr, length);
+    }
+    else {
+        result = malloc(size);
+        if (!result)
+            return NULL;
+        result = mx_memmove(result, ptr, size);
+    }
     return result;
 }
+
