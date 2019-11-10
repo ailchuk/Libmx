2a9fa9a7b352c0f82878cfd7b1ca6aa2ba617820
An <46691148+ailchuk@users.noreply.github.com>
2019-11-10 14:54:45 +0200

    Update mx_memset.c

--- src/mx_memset.c
+++ src/mx_memset.c
@@ -1,7 +1,6 @@
 #include "libmx.h"
 
-void *mx_memset(void *b, int c, size_t len) 
-{
+void *mx_memset(void *b, int c, size_t len)  {
     unsigned char *buf = (unsigned char *)b;
 
     for (size_t i = 0; i < len; i++)
